9215d6afe153187486a9623ca0142773fa04a323
Leonid Malinovsky <leomalinovsky@gmail.com>
2017-07-29 21:22:00 +0300

    task 1.1

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import data from './mock/data';
 
 for (var i = 0; data.length > i; i++){;
 	console.log('=================');
-	for (var key in data[i]){	
-		console.log(key, '=', data[i][key]);
+	for (var key in data[i]){
+		var _key = key;
+		var _val = data[i][key];
+		console.log(_key, '=', _val);
 		
 	}
 }
