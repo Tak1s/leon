ad541d1c9dea836d4730483987e8ef2832244de8
Leonid <leomalinovsky@gmail.com>
2017-09-06 21:55:28 +0300

    jQuery methods added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import data from './mock/data';
+import $ from "jquery";
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+_.map(data, function(value, key) {
+ 		$( "#root" ).get();
+ 		$( "#root" ).append( "<h3> =====" + key + "===== </h3>" );
+ 
+ 	    	_.map(value, function(val, k) {
+  		  		var p = document.createElement("p");
+  		  		$( p ).append( k + "=" + "<span>" + val + "</span>");	  		  		
+  		  		$( "#root" ).append( p );	  		  				
+  			});
+ 	    });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
