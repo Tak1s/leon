914d367fa71ada1e1fe9c5c3186783662b7a0f4a
Leonid <leomalinovsky@gmail.com>
2017-09-07 14:45:06 +0300

    jQuery methods updated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import $ from "jquery";
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 _.map(data, function(value, key) {
- 		$( "#root" ).get();
  		$( "#root" ).append( "<h3> =====" + key + "===== </h3>" );
  
  	    	_.map(value, function(val, k) {
